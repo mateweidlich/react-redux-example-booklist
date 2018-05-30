077eb04c68b99c3d5076d375d0ccf6116d9e2119
Mate Weidlich <mate.weidlich@gmail.com>
2018-05-30 21:30:00 +0200

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+	return [
+		{ title: 'JS book', pages: 101 },
+		{ title: 'Another book', pages: 521 },
+		{ title: 'PHP book', pages: 141 },
+		{ title: 'Some book', pages: 153 }
+	];
+}
